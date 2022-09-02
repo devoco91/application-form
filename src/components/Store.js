794ecf90999807112d753ac719d1e451a6578951
David <abc@gmail.com>
2022-09-01 17:15:58 -1200

    mapped commit

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,83 +1,173 @@
-import React, {useState,useEffect} from 'react'
-import IMG from '../Images/ElizabethVine.png'
-import IMG1 from '../Images/purepng1.png'
-import IMG2 from '../Images/gallery-7.jpg'
+
+import React, { Component } from 'react'
 import './Store.css'
-import axios from 'axios'
+import IMG from '../Images/ElizabethVine.png'
+
+class Store extends Component {
 
 
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       product:[],
+       DataisLoaded:false
+    }
+  }
 
- const url='http://127.0.0.1:8000/product/'
-function Store() {
+  componentDidMount(){
+    fetch(
+      'http://localhost:8000/product/')
+      .then((res)=>res.json())
+      .then((json)=>{
+        this.setState({
+          product:json,
+          DataisLoaded:true
+          
+        })
+      
+      })
+    
+  }
+  render() {
+    const{DataisLoaded, product}=this.state
+    if (!DataisLoaded) return <div>
+      <h1>Please wait for some time....</h1>
+    </div>
 
-  const[product,setProduct]=useState([])
+    return (
+      <div>
+           
 
-  useEffect(() =>{
-    axios
-    .get(url)
-    .then(res =>{
-      console.log(res)
-      setProduct(res.data)
-    })
-
-    .catch(err =>{
-      console.log(err)
-    })
-  }, [])
-
-
-  return (
-    <div>
-     
+          
         <div className="container my-5">
+  
             <div className="row">
-                <div className="col-md-4">
+            { product.map((products) =>(
+           
+             <div className="col-md-4 mb-5" key={products.id}>
+
+          
+                
+            
                   <div className="card text-center">
-                    <img src={IMG} alt="" className='card-img-center' />
+                    <img src={products.image} alt="" className='card-img-center' />
                     <div className="card-body">
                       <div className="card-title">
-                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <h1>{products.name}</h1>
                       </div>
                       <div className="card-text">
-                        <p>&#8358;100000</p>
+                        <p>{products.description}</p>
+                        <p>&#8358;{products.price}</p>
                       </div>
                     </div>
                   </div>
 
+                
+                         
+                 
                 </div>
-                <div className="col-md-4">
-                  <div className="card text-center">
-                    <img src={IMG1} alt="" className='card-img-center' />
-                    <div className="card-body">
-                      <div className="card-title">
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                      </div>
-                      <div className="card-text">
-                        <p>&#8358;700000</p>
-                      </div>
-                    </div>
-                  </div>
+              
+              
 
-                </div>
-                <div className="col-md-4">
-                  <div className="card text-center">
-                    <img src={IMG2} alt="" className='card-img-center' />
-                    <div className="card-body">
-                      <div className="card-title">
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                      </div>
-                      <div className="card-text">
-                        <p>&#8358;168000</p>
-                      </div>
-                    </div>
-                  </div>
+              ))
+            }
+             </div>
+            
+  
+             
+             </div>
 
-                </div>
-            </div>
+
+
         </div>
 
-    </div>
-  )
+     
+    )
+  }
 }
 
 export default Store
+// import React, {useState,useEffect} from 'react'
+// import IMG from '../Images/ElizabethVine.png'
+// import IMG1 from '../Images/purepng1.png'
+// import IMG2 from '../Images/gallery-7.jpg'
+// import './Store.css'
+// import axios from 'axios'
+
+
+
+// //  const url='http://127.0.0.1:8000/product/'
+// function Store() {
+
+//   // const[product,setProduct]=useState([])
+
+//   // useEffect(() =>{
+//   //   axios
+//   //   .get(url)
+//   //   .then(res =>{
+//   //     console.log(res)
+//   //     setProduct(res.data)
+//   //   })
+
+//   //   .catch(err =>{
+//   //     console.log(err)
+//   //   })
+//   // }, [])
+
+
+//   return (
+//     <div>
+     
+//         <div className="container my-5">
+//             <div className="row">
+//                 <div className="col-md-4">
+//                   <div className="card text-center">
+//                     <img src={IMG} alt="" className='card-img-center' />
+//                     <div className="card-body">
+//                       <div className="card-title">
+//                         <h1>Lorem ipsum dolor sit amet.</h1>
+//                       </div>
+//                       <div className="card-text">
+//                         <p>&#8358;100000</p>
+//                       </div>
+//                     </div>
+//                   </div>
+
+//                 </div>
+//                 <div className="col-md-4">
+//                   <div className="card text-center">
+//                     <img src={IMG1} alt="" className='card-img-center' />
+//                     <div className="card-body">
+//                       <div className="card-title">
+//                         <h1>Lorem ipsum dolor sit amet.</h1>
+//                       </div>
+//                       <div className="card-text">
+//                         <p>&#8358;700000</p>
+//                       </div>
+//                     </div>
+//                   </div>
+
+//                 </div>
+//                 <div className="col-md-4">
+//                   <div className="card text-center">
+//                     <img src={IMG2} alt="" className='card-img-center' />
+//                     <div className="card-body">
+//                       <div className="card-title">
+//                         <h1>Lorem ipsum dolor sit amet.</h1>
+//                       </div>
+//                       <div className="card-text">
+//                         <p>&#8358;168000</p>
+//                       </div>
+//                     </div>
+//                   </div>
+
+//                 </div>
+//             </div>
+//         </div>
+
+//     </div>
+//   )
+// }
+
+// export default Store
